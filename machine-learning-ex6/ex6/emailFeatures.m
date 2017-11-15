013f2596f385f3b670b35bb37259a084c5b6d98c
cheryf78 <lcheryf@gmail.com>
2017-11-15 15:30:58 +0100

    Update emailFeatures.m

--- machine-learning-ex6/ex6/emailFeatures.m
+++ machine-learning-ex6/ex6/emailFeatures.m
@@ -48,7 +48,13 @@ x = zeros(n, 1);
 %
 %
 
-
+          for i=1:n
+            if i in word_indices
+                x(i) = 1;
+            else
+                x(i) =0;
+            end
+          end 
 
 
 
